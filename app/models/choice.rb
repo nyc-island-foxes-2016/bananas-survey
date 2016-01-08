137978d225dd1ed33f58bad0e4ba2dc831cd5992
Nicola Beuscher & Zach <pair+deweydell.zlschatz@devbootcamp.com>
2016-01-08 12:50:36 -0500

    Fix choice and user associations

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,5 +1,6 @@
 class Choice < ActiveRecord::Base
-  belongs_to: question_choice
+  belongs_to :question_choice
+  has_many :questions, through: :question_choice
 
   validates :text, length: {maximum: 50}, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,10 @@
 (1..5).to_a.each do |number|
-
 user = User.create(name: "User #{number}", password: "password")
 survey = Survey.create(title: "Survey #{number}", user: user)
 question = Question.create(text: "Question #{number}", survey: survey)
-choice_1 = Choices.create(text: "Choice #{number}" , question: question)
-choice_2 = Choices.create(text: "Choice #{number+1}" , question: question)
-QuestionChoice.create(question: question,choice: choice_1)
-QuestionChoice.create(question: question,choice: choice_2)
-
+choice_1 = Choice.create(text: "Choice #{number}" , questions: [question])
+choice_2 = Choice.create(text: "Choice #{number+1}" , questions: [question])
+QuestionChoice.create(questions: question, choices: choice_1)
+QuestionChoice.create(questions: question, choices: choice_2)
 end
+
