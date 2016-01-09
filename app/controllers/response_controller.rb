abd29f77d2fbe7e8a7cb1e7d94786ef5d10f66a0
Bernadette & Luis Fernando Plaz <pair+bbfrancis.luisplaz@devbootcamp.com>
2016-01-09 17:55:10 -0500

    Fix response save for user created surveys

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -1,9 +1,10 @@
 post '/response' do
-  @question_choice = QuestionChoice.find_by id: params[:response][:choice_id]
-  @question = Question.find_by id: @question_choice.question_id
-  @choice = Choice.find_by id: @question_choice.choice_id
+  @question_choice = QuestionChoice.find_by(choice_id: params[:response][:choice_id])
+  # binding.pry
+  @question = Question.find_by(id: @question_choice.question_id)
+  @choice = Choice.find_by(id: @question_choice.choice_id)
   @survey = Survey.find_by(id: params[:response][:survey_id])
-  @user = User.find_by id: session[:user_id]
+  @user = User.find_by(id: session[:user_id])
   @answer = Response.new(user: @user, survey: @survey, question: @question, choice: @choice)
   if @answer.save
     redirect "/surveys"
