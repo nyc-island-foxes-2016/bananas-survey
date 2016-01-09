dde9b1df63d1821273ddb016e1fb2afd85bf9c3f
Bernadette & Luis Fernando Plaz <pair+bbfrancis.luisplaz@devbootcamp.com>
2016-01-09 12:36:31 -0500

    Add css changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 (1..5).to_a.each do |number|
-user = User.create(name: "User #{number}", password: "password")
+user = User.create(name: "User#{number}", password: "password")
 survey = Survey.create(title: "Survey #{number}", user: user)
 question = Question.create(text: "Question #{number}", survey: survey)
 choice_1 = Choice.create(text: "Choice 1" )
